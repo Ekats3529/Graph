1a372296f410c84df71476bf54b525b1be16c949
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2022-09-12 08:49:39 +0400

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,34 @@
+#pragma once
+class Graph
+{
+	vector<vector<int>> adj_list;
+	int size;
+	vector<int> nodes_list;
+
+	Graph() : size(0) {}
+	Graph(int size, string filename) : size(size){}
+
+	void adjust_node() {
+
+	}
+
+	void adjust_edge() {
+
+	}
+
+	void delete_edge() {
+
+	}
+
+	void delete_node() {
+
+	}
+
+	void print_to_file() {
+
+	}
+
+
+	
+};
+
