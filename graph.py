f17571b2f993c0325b7824fd66f1a131f038064b
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2022-10-10 00:48:36 +0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,7 @@
 class Graph:
     adj_list = {}
+    type = "!directed"
+    nodes_list = []
 
     def __init__(self):
         self.adj_list = {}
@@ -15,14 +17,19 @@ class Graph:
 
     def create_from_file(self, filename):
         adj_list = {}
-        fin = open(filename, encoding="utf8")
+        try:
+            fin = open(filename, encoding="utf8")
+        except FileNotFoundError:
+            print("ERROR: No such file or directory")
+            return False
 
         type = fin.readline()
         n, m = map(int, fin.readline().split())
+        self.nodes_list = fin.readline().split()
         edges = fin.readlines()
 
         for edge in edges:
-            v, u = map(int, edge.split())
+            v, u = edge.split()
 
             if v not in adj_list.keys():
                 adj_list[v] = []
@@ -32,26 +39,77 @@ class Graph:
                 if u not in adj_list.keys():
                     adj_list[u] = []
                 adj_list[u].append(v)
-
+        if len(adj_list.keys()) != n:
+            for node in self.nodes_list:
+                if node not in adj_list.keys():
+                    adj_list[node] = []
         self.adj_list = adj_list
+        return True
 
     def copy(self):
         pass
 
     def add_node(self, node):
-        pass
+        self.adj_list[node] = []
+        self.nodes_list.append(node)
 
     def add_edge(self, edge):
-        pass
+        v, u = edge[0], edge[1]
+        try:
+            self.adj_list[v].append(u)
+        except KeyError:
+            print(f"Unable to add the edge. No such vertex {v}")
+            return False
+
+        if type != "directed":
+            try:
+                self.adj_list[u].append(v)
+            except KeyError:
+                print(f"Unable to add the edge. No such vertex {u}")
+                return False
+        return True
 
     def delete_node(self, node):
-        pass
+        try:
+            for item in self.adj_list.items():
+                if node in item[1]:
+                    del self.adj_list[item[0]][self.adj_list[item[0]].index(node)]
+            del self.adj_list[node]
+            del self.nodes_list[self.nodes_list.index(node)]
+        except KeyError:
+            print(f"ERROR: No such vertex {node}")
+            return False
+        return True
 
     def delete_edge(self, edge):
-        pass
+        v, u = edge[0], edge[1]
+
+        try:
+            del self.adj_list[v][(self.adj_list[v]).index(u)]
+        except KeyError:
+            print(f"ERROR: No such edge ({v}, {u})")
+            return False
+
+        if type != "directed":
+            try:
+                del self.adj_list[u][(self.adj_list[u]).index(v)]
+            except KeyError:
+                print(f"ERROR: No such edge ({v}, {u})")
+                return False
+        return True
 
     def print_to_file(self, filename):
-        fout = open(filename, 'w', encoding="utf8")
+        try:
+            fout = open(filename, 'w', encoding="utf8")
+        except FileNotFoundError:
+            print("ERROR: No such file or directory")
+            return False
+        lst = self.create_edge_list()
+        print(self.type, file=fout)
+        print(len(self.nodes_list), len(lst), file=fout)
+        for edge in lst:
+            print(f"{edge[0]} {edge[1]}", file=fout)
+        return True
 
     def print_to_console(self):
         print(self.adj_list)
